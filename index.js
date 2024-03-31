601e12d06da5b32d47c23e9ecbd22f8a73928871
Arsalan Ahmed <arskhatri2008@gmail.com>
2024-03-31 17:24:58 +0500

    continue

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 import inquirer from "inquirer";
 let todos = [];
-let questions = inquirer.prompt([
-    {
-        name: "todo",
-        type: "input",
-        message: "What you want to add in your todos?",
-    },
-    {
-        name: "addmore",
-        type: "confirm",
-        message: "Do you want to add more?",
-    }
-]);
+let condition = true;
+while (condition) {
+    let addTasks = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "input",
+            message: "What you want to add in your todos?",
+        },
+        {
+            name: "addmore",
+            type: "confirm",
+            message: "Do you want to add more?",
+            default: "false",
+        },
+    ]);
+    todos.push(addTasks.todo);
+    console.log(todos);
+    condition = addTasks.addmore;
+}
--- index.ts
+++ index.ts
@@ -1,18 +1,23 @@
-import inquirer from "inquirer"
+import inquirer from "inquirer";
 
-let todos = []
+let todos = [];
+let condition = true
 
-let addTasks = await inquirer.prompt([
+while (condition) {
+  let addTasks = await inquirer.prompt([
     {
-        name: "todo",
-        type: "input",
-        message: "What you want to add in your todos?",
+      name: "todo",
+      type: "input",
+      message: "What you want to add in your todos?",
     },
     {
-        name: "addmore",
-        type: "confirm",
-        message: "Do you want to add more?",
-        default: "false",
-    }
-]);
-
+      name: "addmore",
+      type: "confirm",
+      message: "Do you want to add more?",
+      default: "false",
+    },
+  ]);
+  todos.push(addTasks.todo);
+  console.log(todos);
+  condition = addTasks.addmore;
+}
